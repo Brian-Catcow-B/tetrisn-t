b1171b999950880d390a9a5f88232253aafff15f
Brian Billman (Ubuntu20 Desktop) <brianjbillman@gmail.com>
2020-07-05 14:14:51 -0400

    it finally actually works

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,14 @@ use ggez;
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::event::{self, EventHandler};
 use ggez::graphics::{self, Color, DrawParam};
-// use cgmath;
-// use ggez::nalgebra as na;
-use mint::Point2;
-use mint::Vector2;
+use ggez::nalgebra as na;
+use na::Point2;
+use na::Vector2;
 
 // file systems stuff
 use std::path;
 use std::env;
 use std::io::{Read, Write};
-
-// type Point2 = cgmath::Point2<f32>;
-// type Vector2 = cgmath::Vector2<f32>;
 
 // this constant is for the two unseen columns above the board so that when an I piece is rotated
 // right after spawning, the two tiles that go above the board are kept track of
@@ -73,8 +69,8 @@ impl Tile {
     pub fn modify_fill(&mut self, active: bool, player: u8) -> Self {
         Self {
             empty: false,
-            active: self.active,
-            player: self.player,
+            active: active,
+            player: player,
         }
     }
 
@@ -91,36 +87,53 @@ struct TileGraphic {
     image: graphics::Image,
 }
 
-const NUM_ROWS: u16 = 8;
+const NUM_PIXEL_ROWS: u16 = 8;
 const GRAY: (u8, u8, u8) = (150u8, 150u8, 150u8);
 const DARK_GRAY: (u8, u8, u8) = (84u8, 84u8, 84u8);
 
 impl TileGraphic {
     pub fn new_empty(ctx: &mut Context) -> Self {
         // create a pixel buffer big enough to hold 4 u8's for each pixel because rgba
-        let mut pixel_buf: [u8; 4 * (NUM_ROWS as usize) * (NUM_ROWS as usize)] = [0u8; 4 * (NUM_ROWS as usize) * (NUM_ROWS as usize)];
-        for row_index in 0..NUM_ROWS {
-            for col_index in 0..NUM_ROWS {
-                if row_index == 0 || row_index == NUM_ROWS - 1 || col_index == 0 || col_index == NUM_ROWS - 1 {
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4) as usize] = DARK_GRAY.0;
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4 + 1) as usize] = DARK_GRAY.1;
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4 + 2) as usize] = DARK_GRAY.2;
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4 + 3) as usize] = 0xff;
+        let mut pixel_buf: [u8; 4 * (NUM_PIXEL_ROWS as usize) * (NUM_PIXEL_ROWS as usize)] = [0u8; 4 * (NUM_PIXEL_ROWS as usize) * (NUM_PIXEL_ROWS as usize)];
+        for row_index in 0..NUM_PIXEL_ROWS {
+            for col_index in 0..NUM_PIXEL_ROWS {
+                if row_index == 0 || row_index == NUM_PIXEL_ROWS - 1 || col_index == 0 || col_index == NUM_PIXEL_ROWS - 1 {
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 0) as usize] = DARK_GRAY.0;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 1) as usize] = DARK_GRAY.1;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 2) as usize] = DARK_GRAY.2;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 3) as usize] = 0xff;
                 } else {
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4) as usize] = GRAY.0;
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4 + 1) as usize] = GRAY.1;
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4 + 2) as usize] = GRAY.2;
-                    pixel_buf[(row_index * NUM_ROWS + col_index * 4 + 3) as usize] = 0xff;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 0) as usize] = GRAY.0;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 1) as usize] = GRAY.1;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 2) as usize] = GRAY.2;
+                    pixel_buf[(row_index * NUM_PIXEL_ROWS * 4 + col_index * 4 + 3) as usize] = 0xff;
                 }
             }
         }
         Self{
-            image: graphics::Image::from_rgba8(ctx, NUM_ROWS, NUM_ROWS, &pixel_buf).expect("Failed to create background tile image"),
+            image: graphics::Image::from_rgba8(ctx, NUM_PIXEL_ROWS, NUM_PIXEL_ROWS, &pixel_buf).expect("Failed to create background tile image"),
         }
     }
 
     pub fn get_size(ctx: &mut Context, board_width: u8, board_height: u8) -> f32 {
-        std::cmp::min(board_height as u32 / graphics::size(ctx).1 as u32, board_width as u32 / graphics::size(ctx).0 as u32) as f32
+        std::cmp::min(graphics::size(ctx).1 as u32 / board_height as u32, graphics::size(ctx).0 as u32 / board_width as u32) as f32
+    }
+
+    pub fn _print_image_buf(self, ctx: &mut Context) {
+        let image_buf: Vec<u8> = self.image.to_rgba8(ctx).expect("Failed to create image buffer");
+        for index in 0..image_buf.len() {
+            if index % 4 == 0 {
+                if index % 32 == 0 {
+                    print!("\n");
+                } else {
+                    if index != 0 {
+                        print!(" ");
+                    }
+                }
+            }
+            print!("{:02x}", image_buf[index]);
+        }
+        print!("\n");
     }
 }
 
@@ -143,57 +156,50 @@ impl Board {
 struct Rustrisnt {
     num_players: u8,
     board: Board,
-    canvas: graphics::Canvas,
+    // canvas: graphics::Canvas,
     text: graphics::Text,
-    // tile_empty_graphic: graphics::Image,
-    tile_empty_batch: graphics::spritebatch::SpriteBatch,
+    tile_size: f32,
+    batch_empty_tile: graphics::spritebatch::SpriteBatch,
 }
 
 impl Rustrisnt {
     pub fn new(mut ctx: &mut Context) -> Rustrisnt {
         // Load/create resources here: images, fonts, sounds, etc.
-        // let image = ggez::graphics::Image::new(&mut ctx, "/backgroundblock.bmp").expect("failed to load image");
-        // image.to_rgba8(&mut ctx);
+        let image = TileGraphic::new_empty(ctx).image;
+        let batch_empty_tile = graphics::spritebatch::SpriteBatch::new(image);
         Self {
             num_players: 2,
-            board: Board::new(14u8, 20u8),
-            canvas: graphics::Canvas::with_window_size(ctx).expect("Failed to create canvas based on window size"),
+            board: Board::new(100u8, 20u8),
+            // canvas: graphics::Canvas::with_window_size(ctx).expect("Failed to create canvas based on window size"),
             text: graphics::Text::new(("Hello world!", graphics::Font::default(), 24.0)),
-            // tile_empty_graphic: ggez::graphics::Image::new(&mut ctx, "/backgroundblock.bmp").expect("Failed to load image"),
-            // tile_empty_graphic: TileGraphic::new_empty().image,
-            tile_empty_batch: graphics::spritebatch::SpriteBatch::new(TileGraphic::new_empty(ctx).image),
-            // tile_empty_batch: graphics::spritebatch::SpriteBatch::new(ggez::graphics::Image::new(&mut ctx, "/backgroundblock.bmp").expect("Failed to load image")),
+            tile_size: TileGraphic::get_size(ctx, 14u8, 20u8),
+            batch_empty_tile: batch_empty_tile,
         }
     }
 }
 
 // draw and update are done every frame
 impl EventHandler for Rustrisnt {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         // Update code here...
+                // println!("Added empty tile");
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, graphics::BLACK);
-
-        // Draw code here...
-        // self.tile_empty_graphic = TileGraphic::new_empty(ctx).image;
-        // self.resize_event(ctx, 100.0, 100.0);
-        // start with a sprite batch
-        // let mut empty_tiles = graphics::spritebatch::SpriteBatch::new(TileGraphic::new_empty(ctx).image);
-        let tile_size = TileGraphic::get_size(ctx, self.board.board_width, self.board.board_height);
+        // empty tiles as the first layer
+        self.tile_size = TileGraphic::get_size(ctx, self.board.board_width, self.board.board_height);
         for x in 0..self.board.board_width {
             for y in 0..self.board.board_height {
                 let x = x as f32;
                 let y = y as f32;
-                let empty_tile = graphics::DrawParam::new()
-                    .dest(Point2::from_slice(&[x * tile_size, y * tile_size]))
-                    .scale(Vector2::from_slice(&[tile_size, tile_size]));
-                self.tile_empty_batch.add(empty_tile);
+                let empty_tile = graphics::DrawParam::new().dest(Point2::new(x * NUM_PIXEL_ROWS as f32, y * NUM_PIXEL_ROWS as f32));
+                self.batch_empty_tile.add(empty_tile);
             }
-        } 
-        graphics::draw(ctx, &self.tile_empty_batch, DrawParam::default())?;
+        }
+        graphics::draw(ctx, &self.batch_empty_tile, DrawParam::new().dest(Point2::new(RELATIVE_WINDOW_WIDTH / 2.0 - (self.tile_size * NUM_PIXEL_ROWS as f32 * self.board.board_width as f32 / (2.0 * 10.0)), 0.0)).scale(Vector2::new(self.tile_size / 10.0, self.tile_size / 10.0)))?;
 
         graphics::present(ctx)
     }
