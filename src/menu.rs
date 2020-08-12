f2c85f073e0735e5ee6d9b85c5ebbd45287f8a40
Brian Billman (Ubuntu20 Desktop) <brianjbillman@gmail.com>
2020-08-11 21:41:06 -0400

    I'd say it's decent; not yet functional tho

--- src/menu.rs
+++ src/menu.rs
@@ -12,7 +12,7 @@ const TEXT_SCALE_DOWN: f32 = 10.0;
 const GRAY: graphics::Color = graphics::Color::new(0.5, 0.5, 0.5, 1.0);
 const SELECT_GREEN: graphics::Color = graphics::Color::new(0.153, 0.839, 0.075, 1.0);
 
-const NUM_MENUOPTION_ENTRIES = 3;
+const NUM_MENUOPTION_ENTRIES: u8 = 3;
 #[repr(u8)]
 enum MenuOption {
     Start,
@@ -68,38 +68,96 @@ impl Menu {
 
     pub fn update(&mut self) -> ProgramState {
         if self.input.keydown_down.1 {
-            
+            self.set_select(self.selection, false);
+            self.selection = (self.selection + 1) % NUM_MENUOPTION_ENTRIES;
+            self.set_select(self.selection, true);
         }
 
+        if self.input.keydown_up.1 {
+            self.set_select(self.selection, false);
+            self.selection = if self.selection == 0 {NUM_MENUOPTION_ENTRIES - 1} else {self.selection - 1};
+            self.set_select(self.selection, true);
+        }
+
+        self.input.was_just_pressed_setfalse();
         ProgramState::Menu
     }
 
-    fn set_select(&mut self, menu_option: MneuOption, select_flag: bool) {
+    fn set_select(&mut self, menu_option: u8, select_flag: bool) {
         match menu_option {
-            Start => {
+            x if x == MenuOption::Start as u8 => {
                 if select_flag {
-                    self.
+                    self.start_text.fragments_mut()[0].color = Some(SELECT_GREEN);
+                } else {
+                    self.start_text.fragments_mut()[0].color = Some(graphics::BLACK);
                 }
             },
-            NumPlayers => {
+            x if x == MenuOption::NumPlayers as u8 => {
                 if select_flag {
-                    self.
+                    self.num_players_text.fragments_mut()[0].color = Some(SELECT_GREEN);
+                } else {
+                    self.num_players_text.fragments_mut()[0].color = Some(graphics::BLACK);
                 }
             },
-            StartingLevel => {
+            x if x == MenuOption::StartingLevel as u8 => {
                 if select_flag {
-                    self.
+                    self.starting_level_text.fragments_mut()[0].color = Some(SELECT_GREEN);
+                } else {
+                    self.starting_level_text.fragments_mut()[0].color = Some(graphics::BLACK);
                 }
             },
+            _ => println!("[!] menu_option didn't find match"),
         }
     }
 
-    pub fn key_down_event(&mut self, keycode: KeyCode, repeat: bool) {
-        println!("hey we are in the keydown for menu. wow!");
+    pub fn key_down_event(&mut self, keycode: KeyCode, _repeat: bool) {
+        if keycode == KeyCode::Left {
+            if !self.input.keydown_left.0 {
+                self.input.keydown_left = (true, true);
+            }
+        } else if keycode == KeyCode::Right {
+            if !self.input.keydown_right.0 {
+                self.input.keydown_right = (true, true);
+            }
+        } else if keycode == KeyCode::Down {
+            if !self.input.keydown_down.0 {
+                self.input.keydown_down = (true, true);
+            }
+        } else if keycode == KeyCode::Up {
+            if !self.input.keydown_up.0 {
+                self.input.keydown_up = (true, true);
+            }
+        // } else if keycode == KeyCode:: {
+        //     if !self.input.keydown_rotate_cw.0 {
+        //         self.input.keydown_rotate_cw = (true, true);
+        //     }
+        // } else if keycode == KeyCode:: {
+        //     if !self.input.keydown_rotate_ccw.0 {
+        //         self.input.keydown_rotate_ccw = (true, true);
+        //     }
+        } else if keycode == KeyCode::Space || keycode == KeyCode::Return {
+            if !self.input.keydown_start.0 {
+                self.input.keydown_start = (true, true);
+            }
+        }
     }
 
     pub fn key_up_event(&mut self, keycode: KeyCode) {
-        println!("hey we are in the keyup for menu. wow!");
+        if keycode == KeyCode::Left {
+            self.input.keydown_left = (false, false);
+        } else if keycode == KeyCode::Right {
+            self.input.keydown_right = (false, false);
+        } else if keycode == KeyCode::Down {
+            self.input.keydown_down = (false, false);
+        } else if keycode == KeyCode::Up {
+            self.input.keydown_up = (false, false);
+        // } else if keycode == KeyCode:: {
+        //     self.input.keydown_rotate_cw = (false, false);
+        // } else if keycode == KeyCode:: {
+        //     self.input.keydown_rotate_ccw = (false, false);
+        } else if keycode == KeyCode::Space || keycode == KeyCode::Return {
+            self.input.keydown_start = (false, false);
+        }
     }
 
     pub fn draw(&mut self, ctx: &mut Context) {
