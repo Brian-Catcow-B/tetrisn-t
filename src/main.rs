c7eb2209e848fcf064ce51e91ce53a21b3676be6
Brian Billman (Ubuntu20 Desktop) <brianjbillman@gmail.com>
2020-09-25 23:13:38 -0400

    custom gamepads; fork ggez

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use ggez::graphics;
 // file systems stuff
 use std::path;
 use std::env;
+use std::fs::File;
+use ggez::filesystem::{resources_dir,};
 
 // rustrisnt files
 mod control;
@@ -14,6 +16,9 @@ mod menu;
 mod game;
 
 mod inputs;
+
+use gilrs;
+use ggez::input::gamepad::GilrsGamepadContext;
 
 fn main() {
     let mut context = ContextBuilder::new("Rustrisn-t", "Catcow")
@@ -25,9 +30,33 @@ fn main() {
         path.push("resources");
         println!("Adding path {:?}", path);
         context = context.add_resource_path(path);
+        // gamecontrollerdb_path = Some(path.join("gamecontrollerdb.txt"));
     }
 
     let (ctx, event_loop) = &mut context.build().expect("[!] Failed to build context");
+
+    // custom controller setup stuffs
+    let mut gilrs_builder = gilrs::GilrsBuilder::new();
+
+    match resources_dir(&ctx).join("gamecontrollerdb.txt").as_path().to_str() {
+        Some(path) => {
+            match std::fs::read_to_string(path) {
+                Ok(string) => {
+                    for line in string.split_ascii_whitespace() {
+                        gilrs_builder = gilrs_builder.add_mappings(line);
+                    }
+                },
+                Err(_) => {
+                    println!("[!] couldn't read contents of {} to string; creating file; no custom controller layouts will function", path);
+                    if let Err(e) = File::create(resources_dir(&ctx).join("gamecontrollerdb.txt")) {
+                        println!("[!] failed to create file {}: {}", resources_dir(&ctx).join("gamecontrollerdb.txt").display(), e);
+                    }
+                },
+            }
+        },
+        None => println!("[!] couldn't obtain path for gamecontrollerdb.txt; no custom controller layouts will function"),
+    }
+    ctx.gamepad_context = Box::new(GilrsGamepadContext::from(gilrs_builder.build().unwrap()));
 
     // set window size
     graphics::set_resizable(ctx, true).expect("[!] Failed to set window to resizable");
